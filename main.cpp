32fc209cc9b4ec77ea993ca5fea85e95e0278d25
florisa <florisagt@gmail.com>
2020-04-28 23:26:30 +0200

    fixing formatation

--- main.cpp
+++ main.cpp
@@ -14,36 +14,41 @@ int main(int argc, char *argv[])
     client.setHostname(hostname);
     client.setPort(port);
 
-    QObject::connect(&client, &QMqttClient::stateChanged, [](QMqttClient::ClientState state){
+    QObject::connect(&client, &QMqttClient::stateChanged, [](QMqttClient::ClientState state)
+    {
             if(state == QMqttClient::Disconnected)
                 qDebug() << " State: Disconnected";
             else if(state == QMqttClient::Connecting)
                 qDebug() << " State: Connecting";
             else if(state == QMqttClient::Connected)
                 qDebug() << " State: Connected";
-        });
+    });
 
-    QObject::connect(&client, &QMqttClient::errorChanged, [](QMqttClient::ClientError error){
+    QObject::connect(&client, &QMqttClient::errorChanged, [](QMqttClient::ClientError error)
+    {
            qDebug() << "not connected" << error;
 
-       });
+    });
 
-    QObject::connect(&client, &QMqttClient::messageReceived, [](const QByteArray &message, const QMqttTopicName &topic){
+    QObject::connect(&client, &QMqttClient::messageReceived, [](const QByteArray &message, const QMqttTopicName &topic)
+    {
            qDebug() << " Received Topic:" << topic.name() << " Message: " << message;
-       });
+     });
 
     QTimer timer;
-    QObject::connect(&timer, &QTimer::timeout, [&client, &topic](){
+    QObject::connect(&timer, &QTimer::timeout, [&client, &topic]()
+    {
            if(client.publish(topic,  QDateTime::currentDateTime().toString().toUtf8()) == -1)
                qDebug() << "Error: Could not publish message";
        });
 
-    QObject::connect(&client, &QMqttClient::connected, [&client, &timer, &filter](){
+    QObject::connect(&client, &QMqttClient::connected, [&client, &timer, &filter]()
+    {
             QMqttSubscription *subscription = client.subscribe(filter);
             if(!subscription)
                 qDebug() << "Could not subscribe";
             timer.start(1000);
-        });
+     });
 
         client.connectToHost();
 
